dd9389cce4421185466dbe9c0149f13d0a5a1810
Kepa <kepatoto@gmail.com>
2023-11-02 20:37:09 -0600

    auto-input: made qol improvments, next ref is auto highlighted, click now changes the color, not space

--- src/Kurdle/Kurdle.tsx
+++ src/Kurdle/Kurdle.tsx
@@ -1,13 +1,13 @@
-import LetterSlot from './LetterSlot';
-import { Color, Position } from './Position';
-import { deepCopy } from '../Utilities/deepCopy';
-import Grid from '@mui/material/Grid';
-import './Kurdle.css';
-import { trimWords, words } from './WordProcessor';
-import React from 'react';
-import Popover from '@mui/material/Popover';
-import Typography from '@mui/material/Typography';
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import LetterSlot from "./LetterSlot";
+import { Color, Position } from "./Position";
+import { deepCopy } from "../Utilities/deepCopy";
+import Grid from "@mui/material/Grid";
+import "./Kurdle.css";
+import { trimWords, words } from "./WordProcessor";
+import React from "react";
+import Popover from "@mui/material/Popover";
+import Typography from "@mui/material/Typography";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 
 type MyState = {
   gridList: Position[][];
@@ -15,86 +15,123 @@ type MyState = {
   anchorEl: HTMLButtonElement | null;
 };
 
+const letterSlotRefs: React.RefObject<HTMLInputElement>[][] = Array.from({ length: 6 }, () =>
+  Array(5).fill(React.createRef<HTMLInputElement>())
+);
+
 function createStartingGrid(): Position[][] {
   let rows: Position[][] = new Array(6);
   for (let index = 0; index < rows.length; index++) {
     let columnPosition: number = 0;
-    rows[index] = Array(5).fill(new Position('', -1, 0)).map((el: Position) => ({ ...el, Index: columnPosition++ }));
+    rows[index] = Array(5)
+      .fill(new Position("", -1, 0))
+      .map((el: Position) => ({ ...el, Index: columnPosition++ }));
   }
+
+  rows.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      letterSlotRefs[rowIndex] = letterSlotRefs[rowIndex] || [];
+      letterSlotRefs[rowIndex][colIndex] = React.createRef<HTMLInputElement>();
+    });
+  });
+
   return rows;
 }
 
 const initialState: MyState = {
   gridList: createStartingGrid(),
   wordList: [],
-  anchorEl: null
-}
+  anchorEl: null,
+};
 
 class Kurdle extends React.Component<any, MyState> {
   constructor(props: any) {
     super(props);
     this.state = {
-      ...initialState
+      ...initialState,
     };
   }
 
   filterWords() {
-    let calculatedWordList = words
+    let calculatedWordList = words;
     let greenLetters: Position[] = [];
 
-    this.state.gridList.forEach(row => {
-      row.forEach(letter => {
-        let overlappingLetters = greenLetters.filter((greenLetter) => greenLetter.Letter === letter.Letter)
+    this.state.gridList.forEach((row) => {
+      row.forEach((letter) => {
+        let overlappingLetters = greenLetters.filter((greenLetter) => greenLetter.Letter === letter.Letter);
         if (overlappingLetters.length > 0) {
           if (letter.Color !== Color.Grey) {
             // If we have a grey letter that is the same as a green then don't filter out words that only have one of that letter.
             //TODOASDF: if we have a yellow, and a green, then we need to only find words where there are both the green and another occurance of the same
-            // letter in the word right now the way I have it written would mean making some changes to how the processing gets done. I am just a little too 
-            // lazy right now to fix that 
-            calculatedWordList = trimWords(letter, calculatedWordList)
+            // letter in the word right now the way I have it written would mean making some changes to how the processing gets done. I am just a little too
+            // lazy right now to fix that
+            calculatedWordList = trimWords(letter, calculatedWordList);
           }
         } else {
-          calculatedWordList = trimWords(letter, calculatedWordList)
+          calculatedWordList = trimWords(letter, calculatedWordList);
         }
 
         if (letter.Color === Color.Green) {
-          greenLetters = greenLetters.concat(letter)
+          greenLetters = greenLetters.concat(letter);
         }
-      })
+      });
     });
 
-    this.setState({ wordList: calculatedWordList })
+    this.setState({ wordList: calculatedWordList });
   }
 
-  updatePositionInfo(
-    row: number,
-    col: number,
-    event: React.FormEvent<HTMLInputElement>,
-  ) {
+  updateColorForPosition(row: number, col: number): void {
     let copiedGridOfLetters: Position[][] = deepCopy(this.state.gridList);
-
-    let finalItemInCell: string = event.currentTarget.value.at(-1) || '';
-
-    let changingColor = finalItemInCell === ' ';
-    if (changingColor) {
-      if (++copiedGridOfLetters[row][col].Color > Color.Green) {
-        copiedGridOfLetters[row][col].Color = Color.Grey;
+    const selectedLetter = copiedGridOfLetters[row][col];
+    if (selectedLetter.Letter !== "") {
+      if (++selectedLetter.Color > Color.Green) {
+        selectedLetter.Color = Color.Grey;
       }
-    } else {
-      copiedGridOfLetters[row][col].Letter = finalItemInCell.toUpperCase();
+      this.setState({ gridList: deepCopy(copiedGridOfLetters) }, this.filterWords);
+    }
+  }
 
-      let setTheInitialColorWhenWeAddALetter = copiedGridOfLetters[row][col].Color === Color.Nothing;
+  updatePositionInfo(row: number, col: number, keyPressed: string) {
+    let copiedGridOfLetters: Position[][] = deepCopy(this.state.gridList);
+    const currentCell = copiedGridOfLetters[row][col];
+    const spacePressed = keyPressed === " ";
+    const shouldNavBack = keyPressed === "Backspace" && currentCell.Letter === "";
+    if (shouldNavBack) {
+      let prevRowIndex = row;
+      let prevColIndex = col - 1;
+
+      if (prevColIndex < 0) {
+        prevColIndex = copiedGridOfLetters[0].length - 1;
+        prevRowIndex = Math.max(prevRowIndex - 1, 0);
+      }
+
+      letterSlotRefs[prevRowIndex][prevColIndex]?.current?.focus();
+    }
+
+    if (keyPressed === "Backspace") {
+      currentCell.Letter = "";
+      currentCell.Color = Color.Nothing;
+    } else if (!spacePressed) {
+      currentCell.Letter = keyPressed.toUpperCase();
+
+      let setTheInitialColorWhenWeAddALetter = currentCell.Color === Color.Nothing;
       if (setTheInitialColorWhenWeAddALetter) {
-        copiedGridOfLetters[row][col].Color = Color.Grey;
+        currentCell.Color = Color.Grey;
+      }
+
+      const nextRowIndex = row;
+      const nextColIndex = col + 1;
+
+      if (nextColIndex < this.state.gridList[nextRowIndex].length) {
+        letterSlotRefs[nextRowIndex][nextColIndex].current?.focus();
+      } else if (nextRowIndex < this.state.gridList.length - 1) {
+        letterSlotRefs[nextRowIndex + 1][0].current?.focus();
       }
     }
 
-    let noTextEntered = event.currentTarget.value.trim().length === 0;
-    if (noTextEntered) {
-      copiedGridOfLetters[row][col].Color = Color.Nothing;
+    if (!spacePressed) {
+      this.setState({ gridList: deepCopy(copiedGridOfLetters) }, this.filterWords);
     }
-
-    this.setState({ gridList: deepCopy(copiedGridOfLetters) }, this.filterWords);
   }
 
   render() {
@@ -107,32 +144,36 @@ class Kurdle extends React.Component<any, MyState> {
     };
 
     const open = Boolean(this.state.anchorEl);
-    const helpButton = open ? 'simple-popover' : undefined;
+    const helpButton = open ? "simple-popover" : undefined;
 
     return (
       <div>
-        <div className='container'>
-          <Popover id={helpButton} open={open} anchorEl={this.state.anchorEl} onClose={handleClose}
-            anchorOrigin={
-              {
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
+        <div className="container">
+          <Popover
+            id={helpButton}
+            open={open}
+            anchorEl={this.state.anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
           >
-            <Typography sx={
-              {
+            <Typography
+              sx={{
                 p: 2,
-                fontSize: '5vw'
-              }}>
-              To change the color of an item press space bar. Press enter or return to go to the next box.
+                fontSize: "5vw",
+              }}
+            >
+              To change the color of an item press click on a box.
             </Typography>
           </Popover>
-          <button
-            onClick={handleClick}
-            className='question-icon'>
-            <HelpOutlineIcon sx={{ 'fontSize': '10vw' }} />
+          <button onClick={handleClick} className="question-icon">
+            <HelpOutlineIcon sx={{ fontSize: "10vw" }} />
           </button>
-          <button className='reset-btn' onClick={() => this.setState({ ...initialState })}>Reset</button>
+          <button className="reset-btn" onClick={() => this.setState({ ...initialState })}>
+            Reset
+          </button>
         </div>
         <div className="div-grid-holder">
           <div>
@@ -140,12 +181,11 @@ class Kurdle extends React.Component<any, MyState> {
               return (
                 <Grid container key={rowIndex}>
                   {row.map((col, colIndex) => (
-                    <Grid item key={colIndex}>
+                    <Grid item key={colIndex} onClick={(_) => this.updateColorForPosition(rowIndex, colIndex)}>
                       <LetterSlot
+                        inputRef={letterSlotRefs[rowIndex][colIndex]}
                         position={this.state.gridList[rowIndex][colIndex]}
-                        onChange={(event: React.FormEvent<HTMLInputElement>) =>
-                          this.updatePositionInfo(rowIndex, colIndex, event)
-                        }
+                        onChange={(keyPressed: string) => this.updatePositionInfo(rowIndex, colIndex, keyPressed)}
                       ></LetterSlot>
                     </Grid>
                   ))}
@@ -153,8 +193,8 @@ class Kurdle extends React.Component<any, MyState> {
               );
             })}
           </div>
-          <div className='wordListDiv'>
-            <p>{this.state.wordList.join(' ')}</p>
+          <div className="wordListDiv">
+            <p>{this.state.wordList.join(" ")}</p>
           </div>
         </div>
       </div>
--- src/Kurdle/LetterSlot.tsx
+++ src/Kurdle/LetterSlot.tsx
@@ -1,33 +1,34 @@
-import { Color, Position } from './Position';
-import './Kurdle.css';
+import { Color, Position } from "./Position";
+import "./Kurdle.css";
 
 type myProps = {
   position: Position;
   onChange: any;
+  inputRef: any;
 };
 
 function colorPicker(Accuracy: number): string {
   switch (Accuracy) {
     case Color.Grey:
-      return '#2c3032';
+      return "#2c3032";
     case Color.Yellow:
-      return '#917f2f';
+      return "#917f2f";
     case Color.Green:
-      return '#42713e';
+      return "#42713e";
     default:
-      return '#121212';
+      return "#121212";
   }
 }
 
 function LetterSlot(props: myProps) {
   return (
     <input
-      className='kurdle-cell'
-      value={props.position.Letter}
-      onChange={(e) => props.onChange(e)}
+      ref={props.inputRef}
+      className="kurdle-cell"
+      onKeyUp={(e) => props.onChange(e.key)}
       style={{
         background: colorPicker(props.position.Color),
-        border: props.position.Color === Color.Nothing ? "2px solid #797063" : "0px"
+        border: props.position.Color === Color.Nothing ? "2px solid #797063" : "0px",
       }}
     ></input>
   );
